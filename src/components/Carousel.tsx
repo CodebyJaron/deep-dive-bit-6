1525790eb0aefa2077385f66b5a939611f5b5ac6
Jaron De Klein <153074306+CodebyJaron@users.noreply.github.com>
2024-02-15 12:54:19 +0100

    push

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import { useState, useEffect, useMemo } from "react";
+import Image from 'next/image';
+import { useState, useEffect, useMemo } from 'react';
 
 interface props {
     headerText: string;
@@ -9,14 +9,7 @@ interface props {
 const Carousel = ({ headerText, headerSubtext }: props) => {
     const [currentSlide, setCurrentSlide] = useState(0);
 
-    const images = useMemo(
-        () => [
-            "/images/slide-1.jpg",
-            "/images/slide-2.jpg",
-            "/images/slide-3.jpeg",
-        ],
-        []
-    );
+    const images = useMemo(() => ['/images/slide-2.jpg', '/images/slide-3.jpeg'], []);
 
     useEffect(() => {
         const intervalId = setInterval(() => {
@@ -29,29 +22,23 @@ const Carousel = ({ headerText, headerSubtext }: props) => {
     }, [images]);
 
     return (
-        <div className="h-[80vh] w-full max-w-full overflow-hidden rounded-b-[20%] flex flex-col justify-center items-center z-0">=======
+        <div className='h-[80vh] w-full max-w-full overflow-hidden rounded-b-[20%] flex flex-col justify-center items-center z-0'>
+            =======
             {images.map((image, index) => (
-                <div
-                    key={index}
-                    className={`carousel-slide ${
-                        index === currentSlide ? "" : "hidden"
-                    }`}
-                >
+                <div key={index} className={`carousel-slide ${index === currentSlide ? '' : 'hidden'}`}>
                     <Image
                         src={image}
                         alt={`Image ${index + 1}`}
-                        className="blur-sm rounded-b-lg brightness-50"
-                        style={{ width: "100%", height: "auto" }}
+                        className='blur-sm rounded-b-lg brightness-50'
+                        style={{ width: '100%', height: 'auto' }}
                         width={1920}
                         height={1080}
                     />
                 </div>
             ))}
-            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
-                <h1 className="text-white text-[4.2rem] font-bold">
-                    {headerText}
-                </h1>
-                <p className="text-gray-300 text-xl">{headerSubtext}</p>
+            <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center'>
+                <h1 className='text-white text-[4.2rem] font-bold'>{headerText}</h1>
+                <p className='text-gray-300 text-xl'>{headerSubtext}</p>
             </div>
         </div>
     );
