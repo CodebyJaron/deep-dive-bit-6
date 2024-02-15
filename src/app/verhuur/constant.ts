fd367aa453ce59208c80cfc4952512989ac208b7
Jaron De Klein <153074306+CodebyJaron@users.noreply.github.com>
2024-02-15 14:59:02 +0100

    Co-authored-by: Pascal Wiersma <pascal@psww.nl>

--- src/app/verhuur/constant.ts
+++ src/app/verhuur/constant.ts
@@ -13,17 +13,76 @@ interface Product {
 
 export const products: Product[] = [
     {
-        name: "Biertafel + banken",
-        price: 80,
-        description:
-            "Biertafel + banken perfect voor al je vrienden om 1 pilsje te drinken.",
-        image: "ja",
+        name: 'Picknicktafel Set',
+        price: 100,
+        description: 'Een complete picknicktafel set, ideaal voor gezellige buitendiners met familie en vrienden.',
+        image: 'ja',
         features: [
             {
-                name: "2 banken",
+                name: '1 tafel',
             },
             {
-                name: "1 tafel",
+                name: '2 banken',
+            },
+        ],
+    },
+    {
+        name: 'Terrasverwarmer',
+        price: 150,
+        description:
+            'Een krachtige terrasverwarmer om zelfs op koele avonden warmte en gezelligheid te bieden tijdens buitenbijeenkomsten.',
+        image: 'ja',
+        features: [
+            {
+                name: 'Verwarmingselement',
+            },
+            {
+                name: 'Instelbare warmtestanden',
+            },
+        ],
+    },
+    {
+        name: 'Opklapbare Partytent',
+        price: 200,
+        description:
+            'Een handige opklapbare partytent om feestjes te organiseren in de tuin of op het strand, ongeacht het weer.',
+        image: 'ja',
+        features: [
+            {
+                name: 'Waterdicht materiaal',
+            },
+            {
+                name: 'Stevig stalen frame',
+            },
+        ],
+    },
+    {
+        name: 'Tuinbarbecue',
+        price: 120,
+        description:
+            'Een robuuste tuinbarbecue, perfect voor het grillen van heerlijke maaltijden en het creëren van mooie herinneringen met vrienden en familie.',
+        image: 'ja',
+        features: [
+            {
+                name: 'Groot kookoppervlak',
+            },
+            {
+                name: 'Verstelbare grillroosters',
+            },
+        ],
+    },
+    {
+        name: 'Hangmat met standaard',
+        price: 90,
+        description:
+            'Een comfortabele hangmat met stevige standaard, ideaal om te ontspannen en tot rust te komen in je tuin of op het terras.',
+        image: 'ja',
+        features: [
+            {
+                name: 'Duurzaam katoenen doek',
+            },
+            {
+                name: 'Stabiele metalen standaard',
             },
         ],
     },
--- src/app/verhuur/page.tsx
+++ src/app/verhuur/page.tsx
@@ -1,52 +1,44 @@
-"use client";
-import Image from "next/image";
-import { products } from "./constant";
-import PrimaryButton from "@/components/common/PrimaryButton";
-import { MdAddShoppingCart, MdKeyboardArrowRight } from "react-icons/md";
+'use client';
+import Image from 'next/image';
+import { products } from './constant';
+import PrimaryButton from '@/components/common/PrimaryButton';
+import { MdAddShoppingCart, MdKeyboardArrowRight } from 'react-icons/md';
+import { FaEye } from 'react-icons/fa';
 
 export default function Verhuur() {
     return (
-        <div className="min-h-full mb-16 px-24">
-            <div className="flex justify-center mt-16">
-                <h1 className="text-primary text-[4.2rem] font-bold">
-                    Verhuur
-                </h1>
+        <div className='min-h-full mb-16 px-24'>
+            <div className='flex flex-col items-center justify-center mt-8 mb-6'>
+                <h1 className='text-primary text-[4.2rem] font-bold'>Verhuur</h1>
+                <p className='text-gray-500'>
+                    Bekijk hier alle producten die wij bij{' '}
+                    <span className='text-primary font-semibold'>Code 0 Producties</span> verhuren!
+                </p>
             </div>
-            <div className="grid lg:grid-cols-3 gap-8 sm:grid-cols-1">
+            <div className='grid lg:grid-cols-3 gap-8 sm:grid-cols-1'>
                 {products.map((product) => {
                     return (
-                        <div
-                            key={product.name}
-                            className="bg-gray-200 rounded-md"
-                        >
-                            <div className="">
+                        <div key={product.name} className='bg-gray-200 rounded-md'>
+                            <div className=''>
                                 <Image
-                                    src={"/images/biertafel.png"}
-                                    alt="bier"
-                                    className="object-fill bg-primary rounded-md"
+                                    src={'/images/biertafel.png'}
+                                    alt='bier'
+                                    className='object-fill bg-primary rounded-md'
                                     width={400}
                                     height={400}
                                 />
                             </div>
 
-                            <div className="flex flex-col px-4 justify-center mt-6">
-                                <h1 className="text-black text-center font-bold text-xl">
-                                    {product.name}
-                                </h1>
+                            <div className='flex flex-col px-4 justify-center mt-6'>
+                                <h1 className='text-black text-center font-bold text-xl'>{product.name}</h1>
 
-                                <div className="px-2 mt-4 flex flex-col justify-center">
+                                <div className='px-2 mt-4 flex flex-col justify-center'>
                                     {product.features.map((productFeature) => {
                                         return (
-                                            <div
-                                                key={productFeature.name}
-                                                className="flex items-center"
-                                            >
-                                                <MdKeyboardArrowRight className="w-4 h-4 text-primary" />
+                                            <div key={productFeature.name} className='flex items-center'>
+                                                <MdKeyboardArrowRight className='w-4 h-4 text-primary' />
 
-                                                <h1
-                                                    key={productFeature.name}
-                                                    className="underline"
-                                                >
+                                                <h1 key={productFeature.name} className='underline'>
                                                     {productFeature.name}
                                                 </h1>
                                             </div>
@@ -54,16 +46,14 @@ export default function Verhuur() {
                                     })}
                                 </div>
 
-                                <div className="mt-4 mb-4 flex justify-between">
-                                    <div className="flex items-center">
-                                        <h1 className="text-black font-bold text-md">
-                                            € {product.price},- p/w
-                                        </h1>
+                                <div className='mt-4 mb-4 flex justify-between'>
+                                    <div className='flex items-center'>
+                                        <h1 className='text-black font-bold text-md'>€ {product.price},- p/w</h1>
                                     </div>
 
-                                    <div className="items-center">
-                                        <PrimaryButton extraClass="py-2 px-6">
-                                            <MdAddShoppingCart className="w-8 h-8" />
+                                    <div className='items-center'>
+                                        <PrimaryButton extraClass='py-2 px-6'>
+                                            <FaEye className='w-8 h-8' />
                                         </PrimaryButton>
                                     </div>
                                 </div>
